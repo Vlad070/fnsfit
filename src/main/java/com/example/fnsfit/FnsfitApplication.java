17e393aeb993c66da9d2e3af6ad71b8cc8c4acd2
Vlad <v.kitsya203@gmail.com>
2022-05-30 14:05:03 +0300

    first commit

---
+++ src/main/java/com/example/fnsfit/FnsfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.fnsfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnsfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fnsfit/FnsfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fnsfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
